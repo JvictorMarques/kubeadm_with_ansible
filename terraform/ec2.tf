f801051bc38180fb70f9c6a1cbf78bcb61a8073e
JvictorMarques <jvictorsilvamarques358@gmail.com>
2025-11-14 09:26:29 -0300

    refactor: clean up security group rules by removing redundant ipv6_cidr_blocks

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -33,17 +33,15 @@ resource "aws_security_group" "nodes_sg" {
   vpc_id = aws_vpc.kubeadm_ansible_vpc.id
 
   ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
